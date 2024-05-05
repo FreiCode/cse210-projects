b17b64be187e0e6138d074d95bb420c95f0b7e12
FreiCode <vicfrei@gmail.com>
2024-05-05 16:24:39 -0500

    prep4 code complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        //Console.WriteLine("Hello Prep4 World!");
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userRespones = Console.ReadLine();
+            userNumber = int.Parse(userRespones);
+
+            //add number to the list if it is not 0
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        //compute the sum of the list
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }  
+        Console.WriteLine($"The sum is: {sum}");
+
+        //compute the average of the list
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //Find the max number in the list
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }  
+        Console.Write($"The max is: {max}");
+        
     }
 }
