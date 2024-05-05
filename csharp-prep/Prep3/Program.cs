0348f345f63dff9b7f3c0654947ffc99125d0f63
FreiCode <vicfrei@gmail.com>
2024-05-05 08:42:56 -0500

    Prep 3 Completion

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Ask user for a magic number
+        //Console.Write("What is the magic number? ");
+        //store number in a variable
+        //string input = Console.ReadLine();
+        //convert string to int
+        //int number = int.Parse(input);
+
+        //have computer generate the random number.
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        //create the guess variable
+        int guess = -1;
+
+        //Create a loop to allow user to guess until they guess the magic number.
+        while (guess != magicNumber)
+        {
+            //Ask the user to guess what the magic number is.
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
